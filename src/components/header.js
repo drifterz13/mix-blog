8443f43a0a2e3ae8dd4dd7f76bbc1c4259e9bc78
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-17 20:31:40 +0700

    change headear

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,59 @@ import styled from "@emotion/styled"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 
-const stacks = ["React", "Node.js", "Gatsby.js", "Html", "CSS"]
+const stacks = ["React", "Node.js", "Javascript", "Html", "CSS"]
+
+const Container = styled("div")`
+  width: calc(100% - 150px);
+  margin: 0 auto;
+  padding: ${rhythm(1)};
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 480px) {
+    width: 100%;
+  }
+`
+
+const LogoContainer = styled("div")`
+  display: flex;
+  align-items: center;
+  align-self: center;
+`
+
+const LogoText = styled("h2")`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  border: 5px solid #639ee2;
+  border-radius: 2px;
+  padding: 8px;
+`
+
+const NavContainer = styled("ul")`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  flex-flow: row wrap;
+  text-transform: uppercase;
+  font-weight: bold;
+  @media (max-width: 480px) {
+    display: none;
+  }
+`
 
 const NavItem = styled("li")`
   padding: 8px 10px;
   height: 37px;
-  width: 120px;
+  margin: 0 ${rhythm(0.5)};
   &:hover {
-    border-bottom: 3px solid yellow;
+    color: #f92301;
     cursor: pointer;
   }
   @media (max-width: 480px) {
@@ -19,90 +64,29 @@ const NavItem = styled("li")`
   }
 `
 
-const LogoContainer = styled("div")`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  align-self: flex-start;
-  margin-bottom: ${rhythm(1)};
-`
-
 const Header = () => (
   <header
     css={css`
-      background: #2a2a2a;
-      height: 300px;
-      color: #fafafa;
+      background: #fff;
+      height: 150px;
+      color: #2a2a2a;
     `}
   >
-    <div
-      css={css`
-        width: calc(100% - 150px);
-        margin: 0 auto;
-        padding: ${rhythm(1)};
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        @media (max-width: 480px) {
-          width: 100%;
-        }
-      `}
-    >
+    <Container>
       <LogoContainer>
-        <div
-          css={css`
-            color: grey;
-            font-size: 50px;
-            font-weight: bold;
-          `}
-        >
-          {"<"}
-          <span
-            css={css`
-              color: salmon;
-              margin-left: 8px;
-            `}
-          >
-            {">"}
-          </span>
-        </div>
-        <h1
-          css={css`
-            text-transform: uppercase;
-            margin: 0 0 0 ${rhythm(1)};
-          `}
-        >
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-            `}
-          >
-            <span>CODE</span>
-            NOTHING
-          </div>
-        </h1>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <LogoText>
+            CODE<span>NOTHING</span>
+          </LogoText>
+        </Link>
       </LogoContainer>
 
-      <ul
-        css={css`
-          list-style: none;
-          margin: 0;
-          display: flex;
-          justify-content: left;
-          align-items: center;
-          width: 100%;
-          height: 40px;
-          flex-flow: row wrap;
-        `}
-      >
+      <NavContainer>
         {stacks.map((stack, index) => (
           <NavItem key={index.toString()}>{stack}</NavItem>
         ))}
-      </ul>
-    </div>
+      </NavContainer>
+    </Container>
   </header>
 )
 
