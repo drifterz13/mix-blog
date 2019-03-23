2ea9fa68c4b0119d4fd78c118337b0db8d4f716a
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-24 04:52:28 +0700

    add sort by date

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = ({ data }) => (
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
