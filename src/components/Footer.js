c3d9e46ccc8dc2b46b9f58b8bbe705a7b5c38297
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-23 13:29:20 +0700

    add subscribe box

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import { FaFacebook, FaTwitterSquare, FaInstagram } from "react-icons/fa"
+import SubscribeBox from "./SubscribeBox"
 
 const iconStyles = css`
   margin: 0 12px;
@@ -13,19 +14,31 @@ const Footer = () => {
       css={css`
         background: #639ee2;
         padding: ${rhythm(1)};
-        height: 200px;
+        height: 300px;
         color: #fff;
         display: flex;
-        justify-content: center;
+        justify-content: space-around;
         align-items: center;
-        flex-direction: column;
+        letter-spacing: 1.8px;
+        @media (max-width: 480px) {
+          height: 400px;
+          flex-direction: column;
+        }
       `}
     >
-      <p>@2019 CodeNothing</p>
-      <div style={{ display: "flex" }}>
-        <FaFacebook css={iconStyles} size="28" />
-        <FaTwitterSquare css={iconStyles} size="28" />
-        <FaInstagram css={iconStyles} size="28" />
+      <SubscribeBox />
+      <div>
+        <p>@2019, CodeNothing</p>
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+          `}
+        >
+          <FaFacebook css={iconStyles} size="28" />
+          <FaTwitterSquare css={iconStyles} size="28" />
+          <FaInstagram css={iconStyles} size="28" />
+        </div>
       </div>
     </footer>
   )
