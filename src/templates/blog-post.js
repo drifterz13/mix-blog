634fcab0d3746e09ae6b133012d8db1fd05ae4b7
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-23 12:28:03 +0700

    fix layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import DisqusThread from "../components/DisqusThread"
