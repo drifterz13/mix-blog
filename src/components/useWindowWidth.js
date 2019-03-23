3e8d9c168f3ccba7abd167767104978eb42947b9
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-23 11:33:25 +0700

    fix build error

--- src/components/useWindowWidth.js
+++ src/components/useWindowWidth.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react"
 
 export function useWindowWidth() {
-  const [width, setWidth] = useState(window.innerWidth)
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth)
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  })
-  return width
+  if (typeof window !== undefined) {
+    const [width, setWidth] = useState(window.innerWidth)
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth)
+      window.addEventListener("resize", handleResize)
+      return () => window.removeEventListener("resize", handleResize)
+    })
+    return width
+  }
+  return false
 }
