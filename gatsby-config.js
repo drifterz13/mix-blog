40572b5b89701c053846c2892d3ca2d2b9719430
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-23 11:23:23 +0700

    edit gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `codenothing`,
+    description: `Blog for Javascript developer.`,
+    author: `@codenothing`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -61,7 +61,6 @@ module.exports = {
     },
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-emotion`,
-    `gatsby-plugin-portal`,
     `gatsby-plugin-catch-links`,
   ],
 }
