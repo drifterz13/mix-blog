dd7e37cbe7d207e11d0d2e5090b0d50ebc8695b3
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-24 10:09:41 +0700

    create Badge

---
+++ src/components/Badge.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { css } from "@emotion/core"
+
+function getBackgroundFromTag(tag) {
+  switch (tag) {
+    case "html":
+      return `#f29300`
+    case "css":
+      return `#639ee2`
+    default:
+      break
+  }
+}
+
+export default ({ tag }) => {
+  return (
+    <div
+      css={css`
+        padding: 0 12px;
+        border-radius: 50px;
+        font-size: 14px;
+        width: 64px;
+        height: 24px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: ${getBackgroundFromTag(tag)};
+        color: #fff;
+        :nth-child(2) {
+            margin-left: 5px;
+        }
+      `}
+    >
+      {`# ${tag}`}
+    </div>
+  )
+}
