f73e358f6ffe4b80c1e11a10cf6b9bd3dc3142db
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-26 22:16:32 +0700

    make scroll always on top

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
 require("prismjs/themes/prism.css")
+
+exports.onRouteUpdate = () => {
+  if (typeof window !== `undefined`) {
+    window.scrollTo(0, 0)
+  }
+}
+
+exports.shouldUpdateScroll = args => {
+  return false
+}
