4f0cb85c433f0015783b734c4f12fb1a46006c39
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-03-31 22:44:13 +0700

    add about content

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,13 +12,37 @@ export default () => (
         margin: ${rhythm(2)} auto 0 auto;
       `}
     >
-      <h1
+      <div
         css={css`
           text-align: center;
+          letter-spacing: 1.3px;
+          b {
+            color: #f92300;
+          }
         `}
       >
-        ABOUT DEV NOTHING
-      </h1>
+        <div>
+          <h1>OUR MISSION</h1>
+          <p>
+            <b>DEV NOTHING</b>{" "}
+            เป็นส่วนหนึ่งของนักพัฒนาเว็บไซต์ไทยที่รักในการแบ่งปันความรู้
+            ประสบการณ์ และความคลั่งไคล้ที่มีต่อ Javascript
+          </p>
+        </div>
+        <div>
+          <h1>OUR VIBE</h1>
+          <p>
+            <b>DEV NOTHING</b> รักในการพัฒนาเว็บไซต์โดยใช้ React.js และ Node.js
+          </p>
+        </div>
+        <div>
+          <h1>OUR PROMISE</h1>
+          <p>
+            <b>DEV NOTHING</b> สัญญาว่าจะเป็นส่วนหนึ่งในการช่วยให้คุณได้ความรู้
+            และไอเดียใหม่ๆไปใช้ในการพัฒนาเว็บไซต์ของคุณ
+          </p>
+        </div>
+      </div>
     </div>
   </Layout>
 )
