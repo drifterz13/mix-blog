fd5bee542b15bd51ffad4ce8fbe52092e7abdf7c
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-04-25 19:07:52 +0700

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `static/images/icon.png`, // This path is relative to the root of the site.
       },
     },
