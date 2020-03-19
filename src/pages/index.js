f2512eaea43344fd3225b94bf735ac91a5373a66
Totsawat Manatsatien <totsawat.m@taskworld.com>
2020-03-19 17:51:03 +0700

    add homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
-import React, { useEffect } from "react"
+import React from "react"
+import { Link } from "gatsby"
+import { MdLibraryBooks, MdBuild } from "react-icons/md"
+import Layout from "../components/layout"
 
-const IndexPage = () => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.location.replace(`${window.location.origin}/blog/1`)
-    }
-  }, [])
-
-  return <div />
+export default function IndexPage() {
+  return (
+    <Layout>
+      <div
+        style={{ maxWidth: "780px" }}
+        className="flex flex-col md:flex-row items-center lg:flex-row my-0 mx-auto md:my-4 lg:my-4"
+      >
+        <Link
+          to="/blog/1"
+          className="flex flex-col items-center justify-center shadow rounded border-4 border-solid border-transparent hover:border-blue-300 h-40 w-56 md:w-1/4 lg:w-1/4 p-6 cursor-pointer"
+        >
+          <MdLibraryBooks className="text-green-500" size={40} />
+          <div className="mt-6 text-lg font-semibold text-gray-800">
+            Articles
+          </div>
+        </Link>
+        <div className="mt-6 md:ml-12 lg:ml-12 md:mt-0 lg:mt-0 flex flex-col items-center justify-center shadow rounded h-40 w-56 md:w-1/4 lg:w-1/4 p-6 cursor-pointer">
+          <MdBuild className="text-gray-800" size={40} />
+          <div className="mt-6 text-lg font-semibold text-gray-800">
+            Developing ...
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
-
-export default IndexPage
